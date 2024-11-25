28174a65ed1f3f984975c0722745e541c978d7f3
FXXFERMI <fermi.fei@mail.utoronto.ca>
2024-11-24 23:30:40 -0500

    added domain

--- E11/scripts.js
+++ E11/scripts.js
@@ -1,6 +1,6 @@
 function getBaseUrl() {
     // return the base URL of your deployed app (only the protocol + domain)
-    return "https://feisiqi.github.io/E11/";
+    return "https://csc-309-e11-fermi.vercel.app/";
   }
   
   function getUTorId() {

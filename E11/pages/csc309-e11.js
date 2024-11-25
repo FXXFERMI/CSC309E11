b3dca4975d8c4be181d6ca9fd88e191a3d6804da
FXXFERMI <fermi.fei@mail.utoronto.ca>
2024-11-24 23:38:20 -0500

    change the path for csc309-e11

--- E11/pages/csc309-e11.js
+++ E11/pages/csc309-e11.js
@@ -1,4 +1,8 @@
-<div id="auto-tester">
-  Hi. I am feisiqi, and I have learned how to develop a full-stack web app
-  and deploy it!
-</div>
+export default function Csc309E11Page() {
+  return (
+    <div id="auto-tester">
+      Hi. I am feisiqi, and I have learned how to develop a full-stack web app
+      and deploy it!
+    </div>
+  );
+}
--- E11/scripts.js
+++ E11/scripts.js
@@ -1,6 +1,7 @@
 function getBaseUrl() {
     // return the base URL of your deployed app (only the protocol + domain)
     return "https://csc-309-e11-fermi.vercel.app/";
+    // return "http://localhost:3000/";
   }
   
   function getUTorId() {
